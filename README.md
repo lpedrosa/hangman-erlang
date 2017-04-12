0d59175e2994dbf0d7ef49a80075590769051349
lpedrosa <lpedrosa88@gmail.com>
2017-04-12 22:31:52 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+hangman
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ src/hangman_game.erl
@@ -0,0 +1,67 @@
+-module(hangman_game).
+
+-export([new/1,
+         guess/2]).
+
+-record(hangman, {word,
+                  word_chars,
+                  tries,
+                  state=continue,
+                  guessed=sets:new()}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+new(Tries) when Tries > 0 ->
+    Word = random_word(words()),
+    WordChars = sets:from_list(Word),
+    #hangman{word = Word, word_chars = WordChars, tries = Tries}.
+
+guess(Char, Game = #hangman{state = continue}) ->
+    NewGame = update_game(Char, Game),
+    {NewGame#hangman.state, NewGame};
+guess(_Char, Game = #hangman{state = GameState}) ->
+    {GameState, Game}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+update_game(Char, Game = #hangman{guessed = Guessed, word_chars = WordChars}) ->
+    % Always add new character to guessed
+    % If the character is not part of the word, guessed will not be
+    % a subset of word_chars
+    NewGuessed = sets:add_element(Char, Guessed),
+    case sets:is_subset(NewGuessed, WordChars) of
+        true ->
+            AllWordsMatch = WordChars == NewGuessed,
+            check_won(AllWordsMatch, Game#hangman{guessed = NewGuessed});
+        false ->
+            NewTries = Game#hangman.tries - 1,
+            check_lost(NewTries, Game)
+    end.
+
+
+check_won(true, Game) ->
+    update_state(won, Game);
+check_won(false, Game) ->
+    update_state(continue, Game).
+
+check_lost(0, Game) ->
+    update_state(lost, Game#hangman{tries = 0});
+check_lost(N, Game) ->
+    update_state(continue, Game#hangman{tries = N}).
+
+update_state(State, Game) ->
+    Game#hangman{state = State}.
+
+
+% Word functions
+
+random_word(Words) when length(Words) > 0 ->
+    Index = rand:uniform(length(Words)),
+    lists:nth(Index, Words).
+
+words() ->
+    ["abacus", "pinapple", "orthopedic", "stuff"].
